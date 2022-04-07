3ddfd1c336b0d6ed88db396a1197b31d3ea08133
M. Mobasher Reza <30533488+nishatreza@users.noreply.github.com>
2022-04-07 17:23:15 +0600

    added login register and fixed issues

--- src/components/dashboard/LandingPage.js
+++ src/components/dashboard/LandingPage.js
@@ -1,75 +1,21 @@
-import React, { Fragment } from 'react'
-import RowCards from './shared/RowCards'
-import StatCards from './shared/StatCards'
-import Campaigns from './shared/Campaigns'
-import { Grid, Card } from '@mui/material'
-import StatCards2 from './shared/StatCards2'
-import DoughnutChart from './shared/Doughnut'
-import UpgradeCard from './shared/UpgradeCard'
-import { styled, useTheme } from '@mui/system'
-import TopSellingTable from './shared/TopSellingTable'
+import { Navigate } from "react-router-dom";
 
-const ContentBox = styled('div')(({ theme }) => ({
-  margin: '30px',
-  [theme.breakpoints.down('sm')]: {
-    margin: '16px',
-  },
-}))
+const LandingPage = ({ user }) => {
+  //   const userDetails = JSON.parse(localStorage.getItem("user-info"));
+  if (!user) return <Navigate to="/login" />;
 
-const Title = styled('span')(() => ({
-  fontSize: '1rem',
-  fontWeight: '500',
-  textTransform: 'capitalize',
-}))
-
-const SubTitle = styled('span')(({ theme }) => ({
-  fontSize: '0.875rem',
-  color: theme.palette.text.secondary,
-}))
-
-const H4 = styled('h4')(({ theme }) => ({
-  fontSize: '1rem',
-  fontWeight: '500',
-  marginBottom: '16px',
-  textTransform: 'capitalize',
-  color: theme.palette.text.secondary,
-}))
-
-const LandingPage = () => {
-  const { palette } = useTheme()
+  const { email, state, role, uid } = user;
 
   return (
-    <Fragment>
-      <ContentBox className="analytics">
-        <Grid container spacing={3}>
-          <Grid item lg={8} md={8} sm={12} xs={12}>
-            <StatCards />
-            <TopSellingTable />
-            <StatCards2 />
-            <H4>Ongoing Projects</H4>
-            <RowCards />
-          </Grid>
+    <div className="landing-div" style={{ textAlign: "center" }}>
+      <h3>Landing page</h3>
+      <br />
+      <h3>User Email: {email}</h3>
+      <h3>User UID: {uid}</h3>
+      <h3>State: {state}</h3>
+      <h3>Role: {role}</h3>
+    </div>
+  );
+};
 
-          <Grid item lg={4} md={4} sm={12} xs={12}>
-            <Card sx={{ px: 3, py: 2, mb: 3 }}>
-              <Title>Traffic Sources</Title>
-              <SubTitle>Last 30 days</SubTitle>
-              <DoughnutChart
-                height="300px"
-                color={[
-                  palette.primary.dark,
-                  palette.primary.main,
-                  palette.primary.light,
-                ]}
-              />
-            </Card>
-            <UpgradeCard />
-            <Campaigns />
-          </Grid>
-        </Grid>
-      </ContentBox>
-    </Fragment>
-  )
-}
-
-export default LandingPage 
+export default LandingPage;
