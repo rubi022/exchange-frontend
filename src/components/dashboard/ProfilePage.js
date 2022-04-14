6cd97b3bdc334b67a5ffa074183800379006282b
M. Mobasher Reza <30533488+nishatreza@users.noreply.github.com>
2022-04-14 20:07:37 +0600

    added the post request buy sell form

--- src/components/dashboard/ProfilePage.js
+++ src/components/dashboard/ProfilePage.js
@@ -5,11 +5,51 @@ import './js/datatables-simple-demo.js';
 import './css/styles.css';
 // import { useEffect } from 'react';
 // import { Helmet } from "react-helmet";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
 
 const ProfilePage = ({ user, setUser }) => {
+  const [market, setMarket] = useState("");
+  const [side, setSide] = useState("");
+  const [ord_type, setOrd_type] = useState("");
+  const [price, setPrice] = useState("");
+  const [volume, setVolume] = useState("");
+
+
+  // submit buy sell form
+  const onSubmitBuySell = async (e) => {
+    e.preventDefault();
+
+    let item = { market, side, ord_type, price, volume };
+
+    console.log(item);
+    let result = await fetch(
+      "https://cp.btfd.cc/api/v2/peatio/market/orders",
+
+      {
+        mode: 'no-cors',
+        method: "POST",
+        body: JSON.stringify(item),
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
+    console.log(result);
+
+    // result = await result.json();
+    // console.log({ result });
+    // console.log(result);
+
+    // if (!result.ok) {
+    //   result = await result.json();
+    //   console.log(result);
+
+
+    // }
+  }
 
 
   // for toggle
@@ -41,8 +81,10 @@ const ProfilePage = ({ user, setUser }) => {
 
 
 
-  console.log(user)
+  // console.log(user);
+  // for the coming user
   const { email, state, role, uid } = user;
+  // state for verify email
   let verfiEmailButtonForPending = false;
 
   // if (state == 'pending') {
@@ -50,6 +92,8 @@ const ProfilePage = ({ user, setUser }) => {
 
     verfiEmailButtonForPending = true;
   }
+
+
 
 
   return (
@@ -312,46 +356,68 @@ const ProfilePage = ({ user, setUser }) => {
                   <div className="col-md-6 offset-md-3 profile-user-form-div">
                     <h3 className="text-center"> One Click Buy/Sell</h3>
 
-                    <form>
+                    <form
+                      onSubmit={onSubmitBuySell}
+                    >
 
                       <div className="form-floating mb-3">
-                        <select className="form-select" id="floatingSelect" aria-label="Floating label select example" defaultValue={'DEFAULT'}>
-                          <option value="DEFAULT" disabled>Choose option</option>
-                          <option value="1">BTC</option>
-                          <option value="2">ETH</option>
-                          <option value="3">AAVE</option>
+                        <select className="form-select" id="floatingSelect"
+                          aria-label="Floating label select example"
+
+                          value={market}
+                          onChange={(e) => setMarket(e.target.value)}
+                          name="market">
+                          <option value="BTC">BTC</option>
+                          <option value="ETH">ETH</option>
+                          <option value="aaveusdt">AAVE</option>
                         </select>
                         <label htmlFor="floatingSelect">Market</label>
                       </div>
                       <div className="form-floating mb-3">
-                        <select className="form-select" id="floatingSelect" aria-label="Floating label select example" defaultValue={'DEFAULT'}>
-                          <option value="DEFAULT" disabled>Choose option</option>
-                          <option value="1">Buy</option>
-                          <option value="2">Sell</option>
+                        <select className="form-select" id="floatingSelect" aria-label="Floating label select example"
+
+                          value={side}
+                          onChange={(e) => setSide(e.target.value)}
+                          name="side">
+                          <option value="buy">Buy</option>
+                          <option value="sell">Sell</option>
                         </select>
                         <label htmlFor="floatingSelect">Order Side</label>
                       </div>
                       <div className="form-floating mb-3">
-                        <select className="form-select" id="floatingSelect" aria-label="Floating label select example" defaultValue={'DEFAULT'}>
-                          <option value="DEFAULT" disabled>Choose option</option>
-                          <option value="1">Limit</option>
-                          <option value="2">Market</option>
+                        <select className="form-select" id="floatingSelect"
+                          aria-label="Floating label select example"
+
+                          value={ord_type}
+                          onChange={(e) => setOrd_type(e.target.value)}
+                          name="ord_type">
+                          <option value="limit">Limit</option>
+                          <option value="market">Market</option>
                         </select>
                         <label htmlFor="floatingSelect">Order Type</label>
                       </div>
 
                       <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Price" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                        <input type="text" className="form-control" placeholder="Price"
+                          aria-label="Recipient's username" aria-describedby="basic-addon2"
+                          value={price}
+                          onChange={(e) => setPrice(e.target.value)}
+                          name="price"
+                        />
                         <span className="input-group-text" id="basic-addon2">USDT</span>
                       </div>
                       <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Amount" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                        <input type="text" className="form-control" placeholder="Amount"
+                          aria-label="Recipient's username" aria-describedby="basic-addon2"
+                          value={volume}
+                          onChange={(e) => setVolume(e.target.value)}
+                          name="volume" />
                         <span className="input-group-text" id="basic-addon2">AAVE</span>
                       </div>
                       <div className="row mb-3">
                         <div className="col-md-12">
                           <ul className="list-group">
-                            <li className="list-group-item">Total  <span className=" float-end">0.000000 USDT</span></li>
+                            <li className="list-group-item">Total  <span className=" float-end">{price * volume}.0000000000 USDT</span></li>
                             <li className="list-group-item">Available  <span className=" float-end">9892.98970000 USDT</span></li>
 
                           </ul>
@@ -362,7 +428,9 @@ const ProfilePage = ({ user, setUser }) => {
                         <div className="col-md-12">
 
                           <div className="d-grid gap-2">
-                            <button className="btn btn-primary" type="button">Buy</button>
+                            {/* <button className="btn btn-primary" type="button">Buy</button> */}
+                            <input type="submit" value="Buy" className="btn btn-primary" />
+
                           </div>
                         </div>
 
