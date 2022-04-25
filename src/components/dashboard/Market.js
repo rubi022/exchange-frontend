d25c3dfbec7791b0b4657174e44fc7dd807efbb8
M. Mobasher Reza <30533488+nishatreza@users.noreply.github.com>
2022-04-25 12:33:39 +0600

    fixed errros api

--- src/components/dashboard/Market.js
+++ src/components/dashboard/Market.js
@@ -24,7 +24,7 @@ const Market = ({ user, setUser }) => {
         let isMounted = true;
 
         async function getMarketData() {
-            const res = await fetch(`${defaultAPI.api.tradeUrl}/market/orders`);
+            const res = await fetch(`${defaultAPI.api.tradeUrl}/public/markets`);
             const data = await res.json();
             if (isMounted) setMarketDetails(data);
 
