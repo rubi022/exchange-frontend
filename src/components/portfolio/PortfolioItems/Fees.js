3828f29015be1c20f2085284df121b0192ef9649
Sohel <tanjim.sohel885@gmail.com>
2022-05-15 12:14:30 +0600

    setup fee component

--- src/components/portfolio/PortfolioItems/Fees.js
+++ src/components/portfolio/PortfolioItems/Fees.js
@@ -3,7 +3,34 @@ import React from "react";
 const Fees = () => {
   return (
     <div>
-      <h4>Fees</h4>
+      <div>
+        <h3 style={{ color: "white", padding: "30px" }}> History</h3>
+        <div>
+          <span>
+            Free (30days) <br /> -
+          </span>
+          <div>
+            <span>Market Fee</span>
+            <span>Taker Fee</span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h3>
+          Trailing Volume <br /> -
+        </h3>
+        <div>
+          <span>over the last 30 days</span>
+        </div>
+      </div>
+      <div>
+        <h3>Wallet Balance</h3>
+        <div>
+          <h3>-</h3>
+          <br />
+          <span> currently held</span>
+        </div>
+      </div>
     </div>
   );
 };
