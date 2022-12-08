f20703b0c7b6926cd234fca61c7a7a5d96d58786
rubi022 <56286981+rubi022@users.noreply.github.com>
2022-12-09 01:49:16 +0800

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,9 @@
 export const defaultAPI = {
     api: {
-        authUrl: 'https://cp.btfd.exchange/api/v2/barong',
-        tradeUrl: 'https://cp.btfd.exchange/api/v2/peatio',
-        applogicUrl: 'https://cp.btfd.exchange/api/v2/applogic',
-        rangerUrl: 'wss://cp.btfd.exchange/api/v2/ranger',
+        authUrl: 'https://api.coins.st/api/v2/barong',
+        tradeUrl: 'https://api.coins.st/api/v2/peatio',
+        applogicUrl: 'https://api.coins.st/api/v2/applogic',
+        rangerUrl: 'wss://api.coins.st/api/v2/ranger',
     },
     minutesUntilAutoLogout: '30',
     withCredentials: false,
