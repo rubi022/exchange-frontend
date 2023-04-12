9a64ca9d0769e37476062c62c1890dcae775a4aa
rubi022 <traderrubi@gmail.com>
2023-04-12 20:04:14 +0800

    Update api.js

--- old-xbt-frontend/src/api/api.js
+++ old-xbt-frontend/src/api/api.js
@@ -1,9 +1,9 @@
 export const defaultAPI = {
     api: {
-        authUrl: 'https://api.coins.st/api/v2/barong',
-        tradeUrl: 'https://api.coins.st/api/v2/peatio',
-        applogicUrl: 'https://api.coins.st/api/v2/applogic',
-        rangerUrl: 'wss://api.coins.st/api/v2/ranger',
+        authUrl: 'https://cp.coins.st/api/v2/barong',
+        tradeUrl: 'https://cp.coins.st/api/v2/peatio',
+        applogicUrl: 'https://cp.coins.st/api/v2/applogic',
+        rangerUrl: 'wss://cp.coins.st/api/v2/ranger',
     },
     minutesUntilAutoLogout: '30',
     withCredentials: false,
